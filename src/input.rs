1597d2e23ebb2f1f35dc09c7d7f3e89eb21d5c48
Louis de Wardt <service@louis.dewardt.uk>
2019-03-31 17:34:26 +0100

    Moved input to own file (#2)
    * Moved input to own file
    
    * Fixed formatting
    
    * Added caching the cargo bin folder

---
+++ src/input.rs
@@ -0,0 +1,101 @@
+use crate::{App, AppState, Config};
+
+use termion::event::Key;
+
+use std::sync::Arc;
+
+pub struct InputManager {
+    on_work_start: Option<String>,
+    on_break_start: Option<String>,
+}
+
+impl InputManager {
+    pub fn new(config: Arc<Config>) -> InputManager {
+        let on_work_start = config.get_string("on_work_start").cloned();
+        let on_break_start = config.get_string("on_break_start").cloned();
+
+        // TODO: Use config for keys
+        InputManager {
+            on_work_start,
+            on_break_start,
+        }
+    }
+
+    /// Handles the input and alters the app accordingly.
+    /// Returns false when the app should stop
+    pub fn handle_input(&self, input: Key, app: &mut App) -> bool {
+        match input {
+            Key::Char('q') | Key::Esc => return false,
+            Key::Char('p') => app.toggle_pause(),
+            Key::Right => {
+                app.transition_to_next_state(std::time::SystemTime::now());
+                handle_next_state(
+                    app.get_state(),
+                    self.on_work_start.as_ref(),
+                    self.on_break_start.as_ref(),
+                );
+            }
+            Key::Left => {
+                let (minutes, seconds) = app.get_time_elapsed();
+
+                if minutes == 0 && seconds < 2 {
+                    app.transition_to_prev_state(std::time::SystemTime::now());
+                    handle_next_state(
+                        app.get_state(),
+                        self.on_work_start.as_ref(),
+                        self.on_break_start.as_ref(),
+                    );
+                } else {
+                    app.reset_timer(false);
+                }
+            }
+            Key::Char('l') => {
+                if app.get_state() == &AppState::ShortBreak {
+                    app.transition_to_state(AppState::LongBreak(true), std::time::SystemTime::now())
+                }
+            }
+            _ => {}
+        }
+
+        true
+    }
+}
+
+pub fn handle_next_state(
+    next_state: &AppState,
+    on_work_start: Option<&String>,
+    on_break_start: Option<&String>,
+) {
+    use std::process::Command;
+    match next_state {
+        AppState::LongBreak(_) => {
+            if let Some(script) = on_break_start {
+                Command::new("sh")
+                    .arg("-c")
+                    .env("BREAK_TYPE", "long")
+                    .arg(script)
+                    .spawn()
+                    .expect("failed to execute break (long) start script");
+            }
+        }
+        AppState::ShortBreak => {
+            if let Some(script) = on_break_start {
+                Command::new("sh")
+                    .arg("-c")
+                    .arg(script)
+                    .env("BREAK_TYPE", "short")
+                    .spawn()
+                    .expect("failed to execute break (short) start script");
+            }
+        }
+        AppState::Work => {
+            if let Some(script) = on_work_start {
+                Command::new("sh")
+                    .arg("-c")
+                    .arg(script)
+                    .spawn()
+                    .expect("failed to execute work start script");
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,11 @@ use crate::config::Config;
 mod ui;
 use ui::Ui;
 
+mod input;
+use input::InputManager;
+
+use std::sync::Arc;
+
 fn main() -> Result<(), failure::Error> {
     // Setup event handlers
     let events = Events::new();
@@ -27,96 +32,34 @@ fn main() -> Result<(), failure::Error> {
             path
         });
 
-    let config =
-        Config::new_from_config_file(config_file_path.as_path()).unwrap_or_else(|_| Config::new());
+    let config = Arc::new(
+        Config::new_from_config_file(config_file_path.as_path()).unwrap_or_else(|_| Config::new()),
+    );
 
     let on_work_start = config.get_string("on_work_start");
     let on_break_start = config.get_string("on_break_start");
 
     // Create default app state
-    let mut app = App::new(&config);
-
+    let mut app = App::new(Arc::clone(&config));
     let mut ui = Ui::new_with_termion()?;
+    let input_manager = InputManager::new(Arc::clone(&config));
 
     loop {
         ui.render(&app)?;
-
-        use termion::event::Key;
         match events.next()? {
-            // TODO: Consider moving the input logic to it's own file
-            Event::Input(input) => match input {
-                Key::Char('q') | Key::Esc => break,
-                Key::Char('p') => app.toggle_pause(),
-                Key::Right => {
-                    app.transition_to_next_state(std::time::SystemTime::now());
-                    handle_next_state(app.get_state(), on_work_start, on_break_start);
+            Event::Input(input) => {
+                if !input_manager.handle_input(input, &mut app) {
+                    // Handle_input has returned false which means that the app should exit
+                    break;
                 }
-                Key::Left => {
-                    let (minutes, seconds) = app.get_time_elapsed();
-
-                    if minutes == 0 && seconds < 2 {
-                        app.transition_to_prev_state(std::time::SystemTime::now());
-                        handle_next_state(app.get_state(), on_work_start, on_break_start);
-                    } else {
-                        app.reset_timer(false);
-                    }
-                }
-                Key::Char('l') => {
-                    if app.get_state() == &AppState::ShortBreak {
-                        app.transition_to_state(
-                            AppState::LongBreak(true),
-                            std::time::SystemTime::now(),
-                        )
-                    }
-                }
-                _ => {}
-            },
+            }
             Event::Tick => {
                 app.update(&|next_state| {
-                    handle_next_state(next_state, on_work_start, on_break_start);
+                    input::handle_next_state(next_state, on_work_start, on_break_start);
                 });
             }
         }
     }
 
     Ok(())
-}
-
-fn handle_next_state(
-    next_state: &AppState,
-    on_work_start: Option<&String>,
-    on_break_start: Option<&String>,
-) {
-    use std::process::Command;
-    match next_state {
-        AppState::LongBreak(_) => {
-            if let Some(script) = on_break_start {
-                Command::new("sh")
-                    .arg("-c")
-                    .env("BREAK_TYPE", "long")
-                    .arg(script)
-                    .spawn()
-                    .expect("failed to execute break (long) start script");
-            }
-        }
-        AppState::ShortBreak => {
-            if let Some(script) = on_break_start {
-                Command::new("sh")
-                    .arg("-c")
-                    .arg(script)
-                    .env("BREAK_TYPE", "short")
-                    .spawn()
-                    .expect("failed to execute break (short) start script");
-            }
-        }
-        AppState::Work => {
-            if let Some(script) = on_work_start {
-                Command::new("sh")
-                    .arg("-c")
-                    .arg(script)
-                    .spawn()
-                    .expect("failed to execute work start script");
-            }
-        }
-    }
 }
